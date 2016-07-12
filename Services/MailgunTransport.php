6c29661d095dc0cc665c4806a6c484c43710a824
Bruno <bruno@yproximite.com>
2016-07-12 10:05:00 +0200

    Catch mailgun exceptions to process all messages (#25)
    * catch Mailgun sendMessage exceptions
    
    * add to address to failed recipients when catching an exception
    
    * added cc and bcc to failed recipients when an exception is catched
    
    * use postData to get recipient list

--- Services/MailgunTransport.php
+++ Services/MailgunTransport.php
@@ -103,14 +103,20 @@ class MailgunTransport implements Swift_Transport
 
         $postData = $this->getPostData($message);
         $domain = $this->getDomain($message);
-        $result = $this->mailgun->sendMessage($domain, $postData, $message->toString());
+        $sent = 1;
+        try {
+            $result = $this->mailgun->sendMessage($domain, $postData, $message->toString());
+        } catch (\Exception $e) {
+            $failedRecipients = $postData['to'];
+            $sent = 0;
+        }
 
         if ($evt) {
             $evt->setResult($result->http_response_code == 200 ? Swift_Events_SendEvent::RESULT_SUCCESS : Swift_Events_SendEvent::RESULT_FAILED);
             $this->eventDispatcher->dispatchEvent($evt, 'sendPerformed');
         }
 
-        return 1;
+        return $sent;
     }
 
     /**
